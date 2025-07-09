7cabb29b549dc4aa96321cb3783d811445338989
Sanjana Bansal <sbansal9347@gmail.com>
2025-07-09 12:34:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js nodes as a newer version
+
+// alert(3+3) we are using nodejs, not browser
+console.log(3
+    +3); // code readability bhi toh achi honi chiye
+console.log("sanjana")
+
+
+let name = "sanjana"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// sring => ""
+// Boolean => true/false 
+// null => standalone value (ek type hai )
+// undefined => abhi value assign nhi hui h
+// symbol => to find unique values
+console.log(typeof null ); //is null ka datatype ka output object aega, joki js ka ek default h,
+console.log(typeof undefined); //iska undefined hi aega.
