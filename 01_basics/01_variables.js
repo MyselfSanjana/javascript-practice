c47b222e94afbb260ec4dff1b10856cc1a86b898
Sanjana Bansal <sbansal9347@gmail.com>
2025-07-09 12:32:50 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 11223344
+let accountEmail = "sanjana@google.com"
+var accountPassword = "1234"
+accountCity = "Delhi"
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
+
+accountEmail = "hello@google.com"
+accountPassword = "6778"
+accountCity = "jaipur"
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
