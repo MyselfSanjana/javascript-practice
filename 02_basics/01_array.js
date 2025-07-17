3a7fd4f97d0845028f18e58b5e45a132521ae7b9
Sanjana Bansal <sbansal9347@gmail.com>
2025-07-17 15:50:57 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,26 +1,26 @@
 const myArray = [0,1,2,3,4,5]
 console.log(myArray)
 
-// //  // array methods 
+//  // array methods 
 
-// //  myArray.push(4)
-// //  console.log(myArray)//[0, 1, 2, 3,4, 5, 4]
-// //  myArray.pop()
-// //  console.log(myArray)//[ 0, 1, 2, 3, 4, 5 ]
+//  myArray.push(4)
+//  console.log(myArray)//[0, 1, 2, 3,4, 5, 4]
+//  myArray.pop()
+//  console.log(myArray)//[ 0, 1, 2, 3, 4, 5 ]
 
-// //  myArray.unshift(6)
-// //  console.log(myArray) //[ 6, 0, 1, 2, 3, 4, 5 ]
+//  myArray.unshift(6)
+//  console.log(myArray) //[ 6, 0, 1, 2, 3, 4, 5 ]
  
-// //  myArray.shift()
-// //  console.log(myArray) //[ 0, 1, 2, 3, 4, 5 ]
+//  myArray.shift()
+//  console.log(myArray) //[ 0, 1, 2, 3, 4, 5 ]
 
-//  console.log(myArray.includes(4)) //true
-//  console.log(myArray.includes(9)) //false
-//  console.log(myArray.indexOf(9)) //-1
+ console.log(myArray.includes(4)) //true
+ console.log(myArray.includes(9)) //false
+ console.log(myArray.indexOf(9)) //-1
 
-//  const newArray = myArray.join()
-//   console.log(myArray) 
-//   console.log(newArray) //0,1,2,3,4,5
+ const newArray = myArray.join()
+  console.log(myArray) 
+  console.log(newArray) //0,1,2,3,4,5
 
 
   // slice and splice //
